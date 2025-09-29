7a1d70948fbe217cfa00736e192407f48a2cb31b
Mete Yuksel <77522027+metekaanyuksel@users.noreply.github.com>
2025-09-29 15:38:42 -0400

    markdown format apparently is; trying github friendly markdown

--- _posts/2025-09-29-I-contain-bird.md
+++ _posts/2025-09-29-I-contain-bird.md
@@ -1,33 +1,23 @@
----
-editor: visual
-title: I contain bird
-toc-title: Table of contents
----
+# I contain bird
+
+2025-09-29
 
 ## Trying this blog thing out
 
-::: cell
-``` {.r .cell-code}
+``` r
 1 + 1
 ```
 
-::: {.cell-output .cell-output-stdout}
     [1] 2
-:::
-:::
 
 Science!
 
 ## Fiddling
 
-::: cell
-``` {.r .cell-code}
+``` r
 plot(c(1,2),c(4,5), type = "l")
 ```
 
-::: cell-output-display
-![](2025-09-29-I-contain-bird_files/figure-markdown/unnamed-chunk-2-1.png)
-:::
-:::
+![](2025-09-29-I-contain-bird.markdown_strict_files/figure-markdown_strict/unnamed-chunk-2-1.png)
 
 ### Galavanting
--- move_md_to_posts.sh
+++ move_md_to_posts.sh
@@ -3,19 +3,19 @@
 QMD_DIR="$HOME/metekaanyuksel.github.io/qmd"
 POSTS_DIR="$HOME/metekaanyuksel.github.io/_posts"
 
+echo "Cleaning $QMD_DIR to keep only .qmd files ..."
+find "$QMD_DIR" -mindepth 1 ! -name '*.qmd' -exec rm -rf {} +
+echo "Non-.qmd files and directories removed from $QMD_DIR."
+
 mkdir -p "$POSTS_DIR"
 
-# Loop through all .qmd files and render to markdown
 for file in "$QMD_DIR"/*.qmd; do
   echo "Rendering $file to markdown..."
-  
-  # Render .qmd to markdown (output in same directory)
+
   quarto render "$file"
-  
-  # Get base filename without extension
+
   basefile=$(basename "$file" .qmd)
-  
-  # Move the generated .md to _posts
+
   if [ -f "$QMD_DIR/$basefile.md" ]; then
     mv "$QMD_DIR/$basefile.md" "$POSTS_DIR/"
     echo "Moved $basefile.md to _posts"
